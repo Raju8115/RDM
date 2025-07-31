4013f1677028642efa3cc154b9aa05c6654a2757
Raju8115 <117435076+Raju8115@users.noreply.github.com>
2025-07-31 16:42:00 +0530

    Update SecurityConfiguration.java

--- manager_ui/backend/src/main/java/com/ibm/managerui/config/SecurityConfiguration.java
+++ manager_ui/backend/src/main/java/com/ibm/managerui/config/SecurityConfiguration.java
@@ -13,7 +13,11 @@ public class SecurityConfiguration {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:5174", "https://rdm-frontend-4-raju-a-dev.apps.rm3.7wse.p1.openshiftapps.com");
+        configuration.addAllowedOrigin("http://localhost:5173");
+        configuration.addAllowedOrigin("http://localhost:5174");
+        configuration.addAllowedOrigin("https://rdm-frontend-2-raju-a-dev.apps.rm3.7wse.p1.openshiftapps.com");
+        configuration.addAllowedOrigin("https://rdm-backend-1-raju-a-dev.apps.rm3.7wse.p1.openshiftapps.com");
+
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
