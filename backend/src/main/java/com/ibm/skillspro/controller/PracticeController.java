34a2a921f23d7f0129765190c31e116fcfd12c4b
Raju8115 <117435076+Raju8115@users.noreply.github.com>
2025-07-31 12:12:11 +0530

    Update PracticeController.java

--- backend/src/main/java/com/ibm/skillspro/controller/PracticeController.java
+++ backend/src/main/java/com/ibm/skillspro/controller/PracticeController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/practices")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://rdm-frontend-2-raju-a-dev.apps.rm3.7wse.p1.openshiftapps.com")
 public class PracticeController {
     @Autowired
     private PracticeService practiceService;
